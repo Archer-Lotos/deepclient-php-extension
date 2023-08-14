37d98600da50f057b4418630d064add8f499cd5c
Viacheslav <novikova835@gmail.com>
2023-08-14 12:13:55 +0300

    add cmake

---
+++ CMakeFiles/deep_client_php_extension.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deep_client_php_extension.dir/deep_client_php_extension.c.o"
+  "CMakeFiles/deep_client_php_extension.dir/deep_client_php_extension.c.o.d"
+  "libdeep_client_php_extension.pdb"
+  "libdeep_client_php_extension.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/deep_client_php_extension.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
